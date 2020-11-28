b8d6d5c26bacd459d7a53937b425c95b7448e8ba
nagai <ewks4769@sugilab.net>
2020-11-28 12:56:10 +0900

    非同期処理のバグを修正

--- js/map.js
+++ js/map.js
@@ -1,48 +1,57 @@
 let markers = [];
 let infoWindows = [];
 
+// let yakkyokuImg = {
+//     url: "./img/pin-icon/yakkyoku-icon.png",
+//     labelOrigin: new google.maps.Point(24, 60)
+// }
+
 // コールバック関数
 function initMap() {
-    map1 = new google.maps.Map(document.getElementById('inasa-map'), {  // あえてlet, const, varをつけずにグローバル変数にした。
-        center: { lat: 34.861887, lng: 137.673219 },
-        zoom: 13,
-        mapTypeControl: false,
-        streetViewControl: false
-    });
-
-    // マーカーおよび情報ウィンドウの配列を生成するforループ
-    let count = 0;
-    for(let h = 0; h < arrayList.length; h++) {
-        for(let i = 0; i < arrayList[h].length; i++, count++) {
-            markers[count] = new google.maps.Marker({
-                position: new google.maps.LatLng(arrayList[h][i].lat, arrayList[h][i].lng),
-                map: map1,
-                // label: arrayList[h][i].label
-            });
-            infoWindows[count] = new google.maps.InfoWindow({
-                content: arrayList[h][i].content,
-                // disableAutoPan: true
-            });
-            console.log(markers.length);
-            markerEvent(count);
-        }
-    }
-
-    let currentInfoWindow = null;
-    // マーカーをクリックしたら情報ウィンドウを表示する関数
-    // (既に情報ウィンドウが出ていた場合はそれを消してから新しい情報ウィンドウを表示する)
-    function markerEvent(arrayIndex) {
-        markers[arrayIndex].addListener('click', function() {
-            if(currentInfoWindow) {
-                currentInfoWindow.close();
-            }
-            infoWindows[arrayIndex].open(map1, markers[arrayIndex]);
-            currentInfoWindow = infoWindows[arrayIndex];
+    setTimeout(function() {
+        map1 = new google.maps.Map(document.getElementById('inasa-map'), {  // あえてlet, const, varをつけずにグローバル変数にした。
+            center: { lat: 34.861887, lng: 137.673219 },
+            zoom: 13,
+            mapTypeControl: false,
+            streetViewControl: false
         });
-    }
 
-    document.getElementById('hit-count').textContent = `引佐町には${markers.length}件の社会資源があります！`;
+        // マーカーおよび情報ウィンドウの配列を生成するforループ
+        let count = 0;
+        for(let h = 0; h < jsonArray.length; h++) {
+            for(let i = 0; i < jsonArray[h].length; i++, count++) {
+                markers[count] = new google.maps.Marker({
+                    position: new google.maps.LatLng(jsonArray[h][i]['緯度'], jsonArray[h][i]['経度']),
+                    map: map1,
+                    // icon: jsonArray[h][i]['アイコンの変数名'],
+                    // label: jsonArray[h][i]['ラベル']
+                });
+                infoWindows[count] = new google.maps.InfoWindow({
+                    content: `<b>${jsonArray[h][i]['名称']}</b><p>${jsonArray[h][i]['住所']}</p><a href="./detail${count}.php" style="color: #5341d3">詳細ページ</a>`,
+                    // disableAutoPan: true
+                });
+                markerEvent(count);
+            }
+        }
+
+        let currentInfoWindow = null;
+        // マーカーをクリックしたら情報ウィンドウを表示する関数
+        // (既に情報ウィンドウが出ていた場合はそれを消してから新しい情報ウィンドウを表示する)
+        function markerEvent(arrayIndex) {
+            markers[arrayIndex].addListener('click', function() {
+                if(currentInfoWindow) {
+                    currentInfoWindow.close();
+                }
+                infoWindows[arrayIndex].open(map1, markers[arrayIndex]);
+                currentInfoWindow = infoWindows[arrayIndex];
+            });
+        }
+
+        document.getElementById('hit-count').textContent = `引佐町には${markers.length}件の社会資源があります！`;
+    }, 100)
 }
+
+// setTimeout(initialize, 100);
 
 $(function() {
     $("#hamburger").click(function() {
@@ -77,51 +86,62 @@ $(function() {
     });
 });
 
-function getCsv(url) {
-    //CSVファイルを文字列で取得。
-    var txt = new XMLHttpRequest();
-    txt.open('get', url, false);
-    txt.send();
+// function getCsv(url) {
+//     //CSVファイルを文字列で取得。
+//     var txt = new XMLHttpRequest();
+//     txt.open('get', url, false);
+//     txt.send();
 
-    //改行ごとに配列化
-    var arr = txt.responseText.split('\n');
+//     //改行ごとに配列化
+//     var arr = txt.responseText.split('\n');
 
-    //1次元配列を2次元配列に変換
-    var res = [];
-    for(var i = 0; i < arr.length; i++){
-        //空白行が出てきた時点で終了
-        if(arr[i] == '') break;
+//     //1次元配列を2次元配列に変換
+//     var res = [];
+//     for(var i = 0; i < arr.length; i++){
+//         //空白行が出てきた時点で終了
+//         if(arr[i] == '') break;
 
-        //","ごとに配列化
-        res[i] = arr[i].split(',');
-    }
+//         //","ごとに配列化
+//         res[i] = arr[i].split(',');
+//     }
 
-    return res;
+//     return res;
+// }
+
+function getExcel(url) {
+    let req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "arraybuffer";
+    req.send();
+
+    let jsonList = [];
+    req.onload = function() {
+        let unit8 = new Uint8Array(req.response);
+        let workbook = XLSX.read(unit8, {type: "array"});
+        let sheet_name_list = workbook.SheetNames;
+        for(let t = 0; t < sheet_name_list.length; t++) {
+            let sheet = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[t]]);
+            jsonList.push(sheet);
+        }
+    };
+    return jsonList;
 }
 
-let yakkyokuCsv = getCsv('./csv/20201112薬局.csv');
-let yakkyokuArray = [];
-for(let x = 3; x < yakkyokuCsv.length; x++) {
-    yakkyokuArray.push({
-        lat: yakkyokuCsv[x][9],
-        lng: yakkyokuCsv[x][10],
-        content: `<b>${yakkyokuCsv[x][0]}</b><p>${yakkyokuCsv[x][2]}</p><a href="./detail.html" style="color: #5341d3">詳細ページ</a>`,
-        dekirukotoArray: yakkyokuCsv[x][11].split('、')
-    })
-}
+let jsonArray = getExcel('../inasa-syakaishigen.xlsm');
+console.log(jsonArray);
 
-let shoutenCsv = getCsv('./csv/20201116商店.csv');
-let shoutenArray = [];
-for(let x = 3; x < shoutenCsv.length; x++) {
-    shoutenArray.push({
-        lat: shoutenCsv[x][14],
-        lng: shoutenCsv[x][15],
-        content: `<b>${shoutenCsv[x][0]}</b><p>${shoutenCsv[x][2]}</p><a href="./detail.html" style="color: #5341d3">詳細ページ</a>`,
-        dekirukotoArray: shoutenCsv[x][16].split('、')
-    })
-}
+// let yakkyokuCsv = getCsv('./csv/20201112薬局.csv');
+// let yakkyokuArray = [];
+// for(let x = 3; x < yakkyokuCsv.length; x++) {
+//     yakkyokuArray.push({
+//         lat: yakkyokuCsv[x][9],
+//         lng: yakkyokuCsv[x][10],
+//         content: `<b>${yakkyokuCsv[x][0]}</b><p>${yakkyokuCsv[x][2]}</p><a href="./detail.html" style="color: #5341d3">詳細ページ</a>`,
+//         dekirukotoArray: yakkyokuCsv[x][11].split('、')
+//     })
+// }
 
-const arrayList = [yakkyokuArray, shoutenArray];
+// const arrayList = [yakkyokuArray, shoutenArray];
 const dekirukotoList = document.form1.dekirukoto;
 
 // チェックボックスで検索する関数
@@ -129,17 +149,19 @@ function dkrSearch() {
     let dkrSearchCount = 0;
     let visibleCount = 0;
     let checkResultUl = '';
-    for(let h = 0; h < arrayList.length; h++) {
-        for(let i = 0; i < arrayList[h].length; i++, dkrSearchCount++) {
-            if(((arrayList[h][i].dekirukotoArray.includes('買い物')) && (dekirukotoList[0].checked)) || ((arrayList[h][i].dekirukotoArray.includes('集う')) && (dekirukotoList[1].checked)) || ((arrayList[h][i].dekirukotoArray.includes('気分転換')) && (dekirukotoList[2].checked)) || ((arrayList[h][i].dekirukotoArray.includes('体を動かす')) && (dekirukotoList[3].checked)) || ((arrayList[h][i].dekirukotoArray.includes('刺激を受ける')) && (dekirukotoList[4].checked)) || ((arrayList[h][i].dekirukotoArray.includes('食事')) && (dekirukotoList[5].checked)) || ((arrayList[h][i].dekirukotoArray.includes('介護・医療サービスを利用する')) && (dekirukotoList[6].checked)) || ((arrayList[h][i].dekirukotoArray.includes('出かける')) && (dekirukotoList[7].checked)) || ((arrayList[h][i].dekirukotoArray.includes('ボランティア')) && (dekirukotoList[8].checked)) || ((arrayList[h][i].dekirukotoArray.includes('相談する')) && (dekirukotoList[9].checked))) {
-                markers[dkrSearchCount].setVisible(true);
-            } else {
-                markers[dkrSearchCount].setVisible(false);
-            }
+    for(let h = 0; h < jsonArray.length; h++) {
+        for(let i = 0; i < jsonArray[h].length; i++, dkrSearchCount++) {
+            if(markers[dkrSearchCount] != null) {
+                if(((jsonArray[h][i]['できること'].includes('買い物')) && (dekirukotoList[0].checked)) || ((jsonArray[h][i]['できること'].includes('集う')) && (dekirukotoList[1].checked)) || ((jsonArray[h][i]['できること'].includes('気分転換')) && (dekirukotoList[2].checked)) || ((jsonArray[h][i]['できること'].includes('体を動かす')) && (dekirukotoList[3].checked)) || ((jsonArray[h][i]['できること'].includes('刺激を受ける')) && (dekirukotoList[4].checked)) || ((jsonArray[h][i]['できること'].includes('食事')) && (dekirukotoList[5].checked)) || ((jsonArray[h][i]['できること'].includes('介護・医療サービスを利用する')) && (dekirukotoList[6].checked)) || ((jsonArray[h][i]['できること'].includes('出かける')) && (dekirukotoList[7].checked)) || ((jsonArray[h][i]['できること'].includes('ボランティア')) && (dekirukotoList[8].checked)) || ((jsonArray[h][i]['できること'].includes('相談する')) && (dekirukotoList[9].checked))) {
+                    markers[dkrSearchCount].setVisible(true);
+                } else {
+                    markers[dkrSearchCount].setVisible(false);
+                }
 
-            if(markers[dkrSearchCount].getVisible()) {
-                visibleCount++;
-                checkResultUl += `<li>${arrayList[h][i].content}</li><hr>`;
+                if(markers[dkrSearchCount].getVisible()) {
+                    visibleCount++;
+                    checkResultUl += `<li><b>${jsonArray[h][i]['名称']}</b><p>${jsonArray[h][i]['住所']}</p><a href="./detail${dkrSearchCount}.php" style="color: #5341d3">詳細ページ</a></li><hr>`;
+                }
             }
         }
     }
@@ -165,15 +187,15 @@ function checkNone() {
 function getCurrentLocation() {
     if(!navigator.geolocation) return;
 
-    var currentLocateImg = {
-        url: "./img/current-location.png",
+    let currentLocateImg = {
+        url: "./img/pin-icon/current-location.png",
         labelOrigin: new google.maps.Point(24, 60)
     }
 
     function success(position) {
-        var currentLatitude  = position.coords.latitude;
-        var currentLongitude = position.coords.longitude;
-        var currentLatlng = new google.maps.LatLng(currentLatitude, currentLongitude);
+        let currentLatitude  = position.coords.latitude;
+        let currentLongitude = position.coords.longitude;
+        let currentLatlng = new google.maps.LatLng(currentLatitude, currentLongitude);
 
         new google.maps.Marker({
             position: currentLatlng,
