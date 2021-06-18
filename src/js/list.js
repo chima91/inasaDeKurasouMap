836d899d82b24c36a56761595273639d33980e9b
Kazuma NAGAI <itb4769we@gmail.com>
2021-06-18 15:26:58 +0900

    不必要な記述を削除

--- src/js/list.js
+++ src/js/list.js
@@ -5,10 +5,6 @@ import { hamburger } from "./hamburger";
 import { endLoading } from "./endLoading";
 import "../scss/list.scss";
 
-$(function() {
-    hamburger();
-});
-
 let jsonArray;
 async function getJson() {
     const res = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=IU_majGmfwhE3mUp_8IbeYXMZGsM-xSgesgOrQlxeRjbEWKzN7lNy_NWu5oeFUl8ERpuU1kNWNH6EBN5xJyYeLTOpHM97A3Im5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnC-NnOpEyNUv90qldEvbLL6Xz1GBq_6Bya6TkKlYhFQE4JamSXLtQT8AI_WHbeHRt2Oqk0FkhV_QGGaU2pP6Ntk&lib=MLUBSv-keSXYUu5ZSNCbXyjlewnNH1PVR');
@@ -43,3 +39,5 @@ getJson()
     .catch(err => {
         console.log(err);
     })
+
+hamburger();
