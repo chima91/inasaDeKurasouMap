a6bc45cec8e377b9104fdc7d54eba758871d951e
Kazuma NAGAI <itb4769we@gmail.com>
2021-07-27 12:00:07 +0900

    jqueryをvanillaに置換

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,25 +1,21 @@
-import $ from "jquery";
-
 export function hamburger() {
-    $("#hamburger").click(function() {
-        $(this).toggleClass("ham-open");
-        if($(this).hasClass("ham-open")) {
-            $("#menu-black-close").css({
-                "z-index":"2",
-                "opacity":".7"
-            })
-        } else {
-            $("#menu-black-close").css({
-                "z-index":"-1",
-                "opacity":"0"
-            })
-        }
-    });
-    $("#menu-black-close").click(function() {
-        $("#hamburger").removeClass("ham-open");
-        $(this).css({
-            "z-index":"-1",
-            "opacity":"0"
-        })
-    });
+  const burger = document.querySelector('#hamburger');
+  const blackClose = document.querySelector('#menu-black-close');
+
+  burger.addEventListener('click', function() {
+    this.classList.toggle('ham-open');
+    if(this.classList.contains('ham-open')) {
+      blackClose.style.zIndex = '2';
+      blackClose.style.opacity = '.7';
+    } else {
+      blackClose.style.zIndex = '-1';
+      blackClose.style.opacity = '0';
+    }
+  });
+
+  blackClose.addEventListener('click', function() {
+    burger.classList.remove('ham-open');
+    this.style.zIndex = '-1';
+    this.style.opacity = '0';
+  });
 }
