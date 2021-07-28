3b8256f2ae370fff8acf73e432296606c61b43b3
Kazuma NAGAI <itb4769we@gmail.com>
2021-07-28 10:42:05 +0900

    「すべてのチェックを外す」処理からjqueryを削除

--- src/js/checkNone.js
+++ src/js/checkNone.js
@@ -1,22 +1,16 @@
-import $ from "jquery";
 import { dekirukotoList } from "./dkrSearch";
 
 // すべて(介護・医療サービスのサブチェック以外)のチェックを外す関数
 export function checkNone() {
-    $("#check-none").on("click", function() {
-        for(let i = 0; i < dekirukotoList.length; i++) {
-            if(i >= 7 && i <= 13) continue;
-            dekirukotoList[i].checked = false;
-        }
-        if($("#checkbox #check7").prop("checked")) {
-            $("#checkbox #sub-check").css({
-                "display":"block"
-            })
-        } else {
-            $("#checkbox #sub-check").css({
-                "display":"none"
-            })
-        }
-    })
+  document.querySelector('#check-none').addEventListener('click', function() {
+    for(let i = 0; i < dekirukotoList.length; i++) {
+      if(i >= 7 && i <= 13) continue;
+      dekirukotoList[i].checked = false;
+    }
 
+    const check7 = document.querySelector('#check7');
+    const subCheck = document.querySelector('#sub-check');
+
+    check7.checked ? subCheck.style.display = 'block' : subCheck.style.display = 'none';
+  });
 }
